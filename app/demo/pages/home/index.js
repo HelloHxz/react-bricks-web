f58b095622f56aaa99a71d2c24c63ab8555eb44b
huxiaozhong <huxiaozhong@sinosafe.com>
2018-04-18 15:34:36 +0800

    '-'

--- app/demo/pages/home/index.js
+++ app/demo/pages/home/index.js
@@ -2,8 +2,39 @@ import {React,PageView,observer} from "react-bricks"
 import './index.less';
 
 class PopLayer extends React.Component{
+
+  refLoad(ref){
+    console.log(ref);
+    ref.onmousewheel = function(event) {
+
+      if(event.wheelDelta<0){
+        if(ref.scrollTop+ref.offsetHeight>=ref.scrollHeight){
+          event.stopPropagation();
+          event.preventDefault();
+        }
+      }else{
+        if(ref.scrollTop<=0){
+          event.stopPropagation();
+          event.preventDefault();
+        }
+      }
+  };
+  ref.addEventListener("DOMMouseScroll", function(event) {
+      if(event.detail>0){
+        if(ref.scrollTop+ref.offsetHeight>=ref.scrollHeight){
+          event.stopPropagation();
+          event.preventDefault();
+        }
+      }else{
+        if(ref.scrollTop<=0){
+          event.stopPropagation();
+          event.preventDefault();
+        }
+      }
+  });
+  }
   render(){
-    return (<div className='p-wrapper'></div>);
+    return (<div className='p-wrapper' ref={this.refLoad.bind(this)}><div style={{width:"2px",height:'1000px',backgroundColor:'green'}}></div></div>);
   }
 }
 
@@ -39,7 +70,7 @@ class HomeScreen extends React.Component {
   render() {
     return <div>home
       <PopLayer/>
-      <div style={{height:'1000px',width:"100px"}}></div>
+      <div style={{height:'1000px',width:"100px",overflow:'auto'}}></div>
       <button onClick={this.go.bind(this)}>Go</button>
     </div>
   }
