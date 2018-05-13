371a638067eccc755a96e955b288efc18bce08f1
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2018-05-13 21:59:55 +0800

    -

--- app/demo/index.js
+++ app/demo/index.js
@@ -3,17 +3,6 @@ import './grid.less';
 
 var config = {
 	root:"home",
-	devConfig:{
-		development:{
-			server:"http://localhost:8000"
-		},
-		uat:{
-			
-		},
-		production:{
-
-		}
-	},
 	pages:{
 		home:require("./pages/home").default,
 		button:require("./pages/button").default,
