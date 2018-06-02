82fe5ab612846ed57d32a0c4a432277454505d7a
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2018-06-02 10:52:45 +0800

    -

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -12,9 +12,21 @@ export default class MessageApi{
 class Message extends React.Component {
     constructor(props){
         super(props);
+        this.state={
+            show:true
+        }
+    }
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({show:false},()=>{
+                setTimeout(()=>{
+                    this.props.parent.destoryMessage(this.props.messageKey);
+                },3000);
+            });
+        },3000);
     }
     render(){
-        return <div className='xz-message-wraper xz-message-wraper-show'><div className='xz-message-wraper-inner'>Message</div></div>
+        return <div className={`xz-message-wraper xz-message-wraper-${this.state.show?'show':'hide'}`}><div className='xz-message-wraper-inner'>Message</div></div>
     }
 }
 
--- src/components/navigator/web/ExtendWrapper.js
+++ src/components/navigator/web/ExtendWrapper.js
@@ -11,42 +11,47 @@ export default class ExtendWrapper extends React.Component{
         xz._setExtendWrapperInstance(this);
         this.componentDict = {};
         this.state = {
-            tmmess:[],
-            tlmes:[],
-            blmes:[],
+            messages:[],
             Modals:[],
         };
     }
     showMessage(params){
         this.seed += 1;
         const messageKey = `message${this.seed}`;
-        this.componentDict[messageKey] = { instance: <Message key={messageKey}/>, pos:'tmmess'};
-        this.state['tmmess'].push(messageKey);
+        this.componentDict[messageKey] = <Message params={params} parent={this} messageKey={messageKey} key={messageKey}/>;
+        this.state.messages.push(messageKey);
         this.setState({
-            tmmess:this.state.tmmess
+            messages:this.state.messages
         });
     }
-    hideMessage(messageKey){
+    destoryMessage(messageKey){
         delete this.componentDict[messageKey];
-    }
-    render(){
-        const messageArr = ['tmmess'];
-        const Re = [];
-        for(let i = 0,j = messageArr.length;i<j;i+=1){
-            const mesWrapperKey=  messageArr[i];
-            const _child = this.state[mesWrapperKey];
-            if(_child.length>0){
-                const messages = [];
-                for(let n = 0,m = _child.length;n<m;n+=1){
-                    const messageKey = _child[n];
-                    const messageInstance = this.componentDict[messageKey].instance;
-                    messages.push(messageInstance);
-                }
-                Re.push(<div key={mesWrapperKey} className={`xz-mes-wraper-${mesWrapperKey}`}>{messages}</div>);
+        for(let i = this.state.messages.length;i>=0;i-=1){
+            if(this.state.messages[i] === messageKey){
+                this.state.messages.splice(i,1);
+                break;
             }
         }
+        this.setState({
+            messages:this.state.messages
+        });
+    }
+    render(){
+        const {messages} = this.state;
+        let messageDom = null;
+        if(messages.length>0){
+            const messageChild = [];
+            for(let n = 0,m = messages.length;n<m;n+=1){
+                const messageKey = messages[n];
+                const messageInstance = this.componentDict[messageKey];
+                if(messageInstance){
+                    messageChild.push(messageInstance);
+                }
+            }
+            messageDom = <div className='xz-mes-container'>{messageChild}</div>
+        }
         return (<React.Fragment>
-            {Re}
+            {messageDom}
             </React.Fragment>);
     }
 }
