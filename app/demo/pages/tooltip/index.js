ee466802d6de5e8b6de712785a5198650a737953
huxiaozhong <huxiaozhong@sinosafe.com>
2018-06-07 16:43:34 +0800

    '-'

--- app/demo/pages/tooltip/index.js
+++ app/demo/pages/tooltip/index.js
@@ -27,6 +27,21 @@ class ToolTipDemo extends React.PureComponent {
        <Tooltip placement='leftbottom'><Button>LeftBottom</Button></Tooltip>
        <Tooltip placement='left'><Button>Left</Button></Tooltip>
        <Tooltip placement='lefttop'><Button>LeftTop</Button></Tooltip>
+
+       <div style={{marginTop:20}}>
+        <Tooltip positionMode='absolute' placement='topleft'><Button>TopLeft</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='top'><Button>Top</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='topright'><Button>TopRight</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='righttop'><Button>RightTop</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='right'><Button>Right</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='rightbottom'><Button>RightBottom</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='bottomright'><Button>BottomRight</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='bottom'><Button>Bottom</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='bottomleft'><Button>BottomLeft</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='leftbottom'><Button>LeftBottom</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='left'><Button>Left</Button></Tooltip>
+        <Tooltip positionMode='absolute' placement='lefttop'><Button>LeftTop</Button></Tooltip>
+       </div>
     </div>
   }
 }
