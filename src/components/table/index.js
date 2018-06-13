7f44026de916506f70605c9771fdcb54c1d0817a
huxiaozhong <huxiaozhong@sinosafe.com>
2018-06-13 11:40:06 +0800

    '-'

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Theme from '../theme';
 import './index.less';
 
+/* 
+    colspan : 当前字段对应的根字段的个数
+    rowspan ：先得到头部行数为rowCount,当前字段从上到下进行分配，可分配数为rowCount，
+    如果该字段有子字段那么分配1，rowspan为1，可分配数减去1，再遍历下去分配。
+    如果没有子字段 那么rowspan为可分配的所剩下的值。
+*/
 
 class TableHeader extends React.Component {
     render(){
