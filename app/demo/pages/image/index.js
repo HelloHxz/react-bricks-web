a6675a067ed33ec15dc76e473e5fd6b54379be91
huxiaozhong <huxiaozhong@sinosafe.com>
2018-06-28 11:18:56 +0800

    '-'

--- app/demo/pages/image/index.js
+++ app/demo/pages/image/index.js
@@ -24,15 +24,18 @@ class ImageDemo extends React.PureComponent {
 
   render() {
     const image1Name = '/timg.jpg';
-    const img = require('../../imgs' + image1Name);
+    const image2Name = '/1.jpg';
+    const path = '../../imgs';
+    const img = require('../../imgs'+ image1Name);
+    const img2 = require('../../imgs' + image2Name);
     return <div style={{height:'100%',overflow:'auto'}}>
         <Image style={{width:200,height:400}} resizeMode="contain" src={img}/>
         <Image style={{width:200,height:400}} resizeMode="stretch" src={img}/>
         <Image style={{width:200,height:400}} resizeMode="cover" src={img}/>
 
-        <Image style={{width:330,height:400}} resizeMode="contain" src={require('../../imgs/1.jpg')}/>
-        <Image style={{width:330,height:400}} resizeMode="stretch" src={require('../../imgs/1.jpg')}/>
-        <Image style={{width:330,height:400}} resizeMode="cover" src={require('../../imgs/1.jpg')}/>
+        <Image style={{width:330,height:400}} resizeMode="contain" src={img2}/>
+        <Image style={{width:330,height:400}} resizeMode="stretch" src={img2}/>
+        <Image style={{width:330,height:400}} resizeMode="cover" src={img2}/>
     </div>
   }
 }
