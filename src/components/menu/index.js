9488af4acd5fa2041df25a72ada2153f0b48bd73
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2018-07-08 15:06:11 +0800

    -

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -16,6 +16,7 @@ class Menu extends React.Component{
             const urlInfo = XZ.router.getUrlInfo();
             selectedKey = urlInfo.path;
         }
+        this.selectedOpenKeyArray = [];
         this.state={
             selectedKey:selectedKey||props.selectedKey
         }
@@ -57,7 +58,11 @@ class Menu extends React.Component{
         for(var i=0,j=data.length;i<j;i++){
             var itemData = data[i];
             if(itemData.children){
-                children.push(<MenuSection open={true} menu={this} onItemClick={this.onItemClick.bind(this)} level={0} key={i} data={itemData}/>);
+                const p={level:0,key:i,data:itemData,menu:this};
+                if(this.selectedOpenKeyArray.indexOf(itemData.key)>=0){
+                    p.open = true;
+                }
+                children.push(<MenuSection {...p} onItemClick={this.onItemClick.bind(this)} />);
             }else{
                 children.push(<MenuSectionItem menu={this} onItemClick={this.onItemClick.bind(this)} level={0} key={i} data={itemData}/>);
             }
@@ -74,14 +79,17 @@ class Menu extends React.Component{
             }else{
                 
             }
-            itemData.__parentKey = parentKey;
+            // itemData.__parentKey = parentKey;
+            if(itemData.key===this.state.selectedKey){
+                this.selectedOpenKeyArray = parentKey;
+            }
         }
     }
     render(){
         let children = [];
         const p = {};
+        
         this._processData(this.props.data,[]);
-        console.log(this.props.data.toJS());
         if(this.props.collapsed === true){
             children = this._getMiniVerticalItems(this.props.data);
             p.className = 'xz-menu xz-menu-vertical-mini';
@@ -107,17 +115,21 @@ class MenuSection extends React.Component{
             open:props.open,
         }
     }
+    componentDidMount(){
+    }
     headerClick(){
         if(this.timeid||this.timeid2){
             return;
         }
-        if(this.state.open){
+        let openState = !this.state.open;
+        if(!openState){
+            //关闭的时候 从auto变成固定的高度 才有动画
             this.groupouter.style.height =  this.groupinner.offsetHeight+"px";
         }
         this.timeid = setTimeout(()=>{
             this.timeid = null;
             this.setState({
-                open:!this.state.open
+                open:openState
             },()=>{
                 if(this.state.open){
                       this.timeid2 = setTimeout(()=>{
@@ -130,8 +142,17 @@ class MenuSection extends React.Component{
             });
         },50);
     }
+  
     outerLoad(groupouter){
         this.groupouter = groupouter;
+        if(this.groupouter){
+            if(this.state.open){
+                // 初始化的时候 保证高度不被固定
+                setTimeout(()=>{
+                    this.groupouter.style.height = "auto"
+                },210);
+            }
+        }
     }
     innerLoad(groupinner){
         this.groupinner = groupinner;
@@ -149,7 +170,11 @@ class MenuSection extends React.Component{
         for(var i=0,j=this.state.data.children.length;i<j;i++){
             var itemData = this.state.data.children[i];
             if(itemData.children){
-                children.push(<MenuSection menu={this.props.menu} onItemClick={this.props.onItemClick} level={this.props.level+1} key={i} data={itemData}/>);
+                const p={level:this.props.level+1,key:i,data:itemData,menu:this.props.menu};
+                if(this.props.menu.selectedOpenKeyArray.indexOf(itemData.key)>=0){
+                    p.open = true;
+                }
+                children.push(<MenuSection onItemClick={this.props.onItemClick} {...p}/>);
             }else{
                 children.push(<MenuSectionItem menu={this.props.menu} onItemClick={this.props.onItemClick} level={this.props.level+1} key={i} data={itemData}/>);
             }
@@ -187,10 +212,15 @@ class MenuSectionItem extends React.Component{
         if(this.props.data.icon){
             icon = <i className={`${this.props.data.icon} xz-menu-icon`}/>
         }
-        if(this.props.menu.state.selectedKey===this.props.data.key){
+        const isSelected = this.props.menu.state.selectedKey===this.props.data.key;
+        if(isSelected){
             className.push('xz-menu-item-selected');
         }
-        return (<div onClick={this.itemClick.bind(this)} className={className.join(' ')} style={{paddingLeft:paddingLeft}}>
+        return (<div ref={(root)=>{this.root = root;
+            if(root&&isSelected){
+                this.root.scrollIntoViewIfNeeded();
+            }
+        }} onClick={this.itemClick.bind(this)} className={className.join(' ')} style={{paddingLeft:paddingLeft}}>
             {icon}
             {this.props.data.label}
         </div>);
