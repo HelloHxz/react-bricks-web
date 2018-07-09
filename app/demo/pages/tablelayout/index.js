cce3f4298526f2c8a2e51bb76d42e8e4e446925a
huxiaozhong <huxiaozhong@sinosafe.com>
2018-07-09 17:32:19 +0800

    '-'

--- app/demo/pages/tablelayout/index.js
+++ app/demo/pages/tablelayout/index.js
@@ -1,4 +1,5 @@
-import {React,PageView,observer,PageContainer,Modal,Button,message,TableLayout} from "react-bricks-web"
+import {React,PageView,observer,PageContainer,Modal,Button,message,TableLayout} from "react-bricks-web";
+import './index.less';
 
 @PageView()
 class TableLayoutDemo extends React.PureComponent {
@@ -17,18 +18,18 @@ class TableLayoutDemo extends React.PureComponent {
 
   render() {
     return <div>
-        <TableLayout style={{height:100}}>
-            <TableLayout.Cell style={{width:150,backgroundColor:'red'}}>width:150px</TableLayout.Cell>
+        <TableLayout className='demo-tablelayout' style={{height:100}}>
+            <TableLayout.Cell style={{width:150,}}>width:150px</TableLayout.Cell>
             <TableLayout.Cell>auto</TableLayout.Cell>
-            <TableLayout.Cell style={{width:100,backgroundColor:'yellow'}}>width:100px</TableLayout.Cell>
-            <TableLayout.Cell style={{width:70,backgroundColor:'green'}}>width:70px</TableLayout.Cell>
+            <TableLayout.Cell style={{width:100,}}>width:100px</TableLayout.Cell>
+            <TableLayout.Cell style={{width:70,}}>width:70px</TableLayout.Cell>
         </TableLayout>
         <br/>
-        <TableLayout>
-            <TableLayout.Cell style={{width:150,backgroundColor:'red'}}>width:150px</TableLayout.Cell>
+        <TableLayout className='demo-tablelayout'>
+            <TableLayout.Cell style={{width:150,}}>width:150px</TableLayout.Cell>
             <TableLayout.Cell>auto</TableLayout.Cell>
-            <TableLayout.Cell style={{backgroundColor:'green'}}>auto</TableLayout.Cell>
-            <TableLayout.Cell style={{width:70,backgroundColor:'yellow'}}>width:70px</TableLayout.Cell>
+            <TableLayout.Cell>auto</TableLayout.Cell>
+            <TableLayout.Cell style={{width:70,}}>width:70px</TableLayout.Cell>
         </TableLayout>
     </div>
   }
