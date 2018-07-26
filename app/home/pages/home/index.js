fed30f6aa443d45215002520dde1248a96b50079
huxiaozhong <huxiaozhong@sinosafe.com>
2018-07-26 11:36:01 +0800

    '-'

--- app/home/pages/home/index.js
+++ app/home/pages/home/index.js
@@ -1,5 +1,6 @@
-import {React,PageView,Menu,PopView,Input,Form,Select,RouteView,Row,Col,Modal,Button,VBox,HBox,XZ} from "react-bricks-web"
+import {React,PageView,Image,Tabs,Input,Form,Select,RouteView,Row,Col,Modal,Button,VBox,HBox,XZ} from "react-bricks-web"
 import './index.less';
+import BkImage from '../../imgs/bk.jpg';
 
 @PageView()
 class HomeScreen extends React.PureComponent {
@@ -14,7 +15,21 @@ class HomeScreen extends React.PureComponent {
 
   render() {
     return (
-        <div>Home</div>
+      <div>
+        <Tabs 
+            defaultSelectedKey='home'
+            size='lg'
+            data={[
+                {label:"首页",key:'home'},
+                {label:"React Bricks Web",key:'email'},
+                {label:"React Bricks Native",key:'info'},
+           ]} />
+        <div className='home-top'>
+          <Image style={{ width:300,height:300 }} src={BkImage} resizeMode="contain" />
+          <div className='title-content'><span className='main-title'>拥抱React 拥抱全端</span></div>
+        </div>
+        <div className='home-body'></div>
+      </div>
     );
   }
 }
