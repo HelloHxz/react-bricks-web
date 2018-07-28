d6ff8231f70e5d04d501f81b49520c18ff5ed41a
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2018-07-28 15:57:06 +0800

    -

--- app/home/pages/home/components/home.js
+++ app/home/pages/home/components/home.js
@@ -46,7 +46,8 @@ class HomeScreen extends React.PureComponent {
                 <span className='class-card-status'>实验中</span>
                   <span className='class-card-title'>Bricks Mobile Web & Native</span>
                   <p>
-                       一套代码用于构建IOS,Android,Mobile Web的组件库,采用<a href='https://reactjs.org/'>React</a>/
+                       一套代码用于构建IOS,Android,Mobile Web的组件库,采用
+                       <a href='https://reactjs.org/'>React</a>/
                        <a href='https://facebook.github.io/react-native/'>React Native</a>/
                        <a href='https://mobx.js.org/'>Mobx</a>作为开发技术栈
                   </p>
@@ -58,14 +59,18 @@ class HomeScreen extends React.PureComponent {
               </Row.Col>
               <Row.Col span={{sm:24,md:12,lg:6}}>
                 <div className='class-card'>
-                <span className='class-card-status'>策划中</span>
+                <span className='class-card-status'>规划中</span>
                   <span className='class-card-title'>Bricks Desktop</span>
                   <p>
-                      用于构建PC端中后台管理系统,包含丰富的组件让您快速轻松的开发体验. 采用React/Mobx作为开发技术栈
+                      结合Bricks PC Web用于构建PC客户端应用,采用
+                      <a href='https://reactjs.org/'>React</a>/
+                      <a href='https://electronjs.org/'>Electron</a>/
+                      <a href='https://mobx.js.org/'>Mobx</a>
+                      作为技术栈.
                   </p>
                   <div className='class-card-bottom'>
-                    <Button type='primary'>Demo</Button>
-                    <Button>github</Button>
+                    <Button disabled type='primary'>Demo</Button>
+                    <Button disabled>github</Button>
                  </div>
                 </div>
               </Row.Col>
@@ -77,15 +82,42 @@ class HomeScreen extends React.PureComponent {
                       使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批,通讯录...等企业管理应用
                   </p>
                   <div className='class-card-bottom'>
-                    <a href='./index.html'><Button type='primary'>Demo</Button></a>
-                    <Button>github</Button>
+                    <Button disabled type='primary'>Demo</Button>
+                    <Button disabled>github</Button>
                  </div>
                 </div>
               </Row.Col>
             </Row>
            </div>
         </div>
-        <div className='feature-warpper'></div>
+        <div className='feature-warpper'>
+            <div className='right-tri-bk'></div>
+            <p className='feature-title'>重复的组件化</p>
+            <span className='feature-desc'>使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            </span>
+        </div>
+        <div className='feature-warpper bw'>
+            <div className='right-tri-bk'></div>
+            <p className='feature-title'>组件的最佳实践化</p>
+            <span className='feature-desc'>使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            </span>
+        </div>
+        <div className='feature-warpper'>
+            <div className='right-tri-bk'></div>
+            <p className='feature-title'>我们的定位</p>
+            <span className='feature-desc'>使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            </span>
+        </div>
+        <div className='feature-warpper bw'>
+            <div className='right-tri-bk'></div>
+            <p className='feature-title'>我们想的更多</p>
+            <span className='feature-desc'>使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            使用Bricks UI 开发的全端的超级DEMO,以IM为核心,包括企业动态,审批
+            </span>
+        </div>
         <div className='footer'></div>
         </React.Fragment>
     );
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -23,19 +23,25 @@ export default class Button extends React.Component{
     }
     render(){
         var p = {};
-        if(this.props.onMouseLeave){
-            p.onMouseLeave = this.onMouseLeave.bind(this);
+        var className = [`xz-btn xz-btn-size-${Theme.getConfig('size',this.props)} xz-btn-type-${Theme.getConfig('btn-type',this.props)}`];
+        if(!this.props.disabled){
+            if(this.props.onMouseLeave){
+                p.onMouseLeave = this.onMouseLeave.bind(this);
+            }
+            if(this.props.onMouseOver){
+                p.onMouseOver = this.onMouseOver.bind(this);
+            }
+            if(this.props.onClick){
+                p.onClick = this.onClick.bind(this);
+            }
+        }else{
+            className.push('xz-btn-disabled');
         }
-        if(this.props.onMouseOver){
-            p.onMouseOver = this.onMouseOver.bind(this);
-        }
-        if(this.props.onClick){
-            p.onClick = this.onClick.bind(this);
-        }
+     
         if(this.props.style){
             p.style = this.props.style;
         }
-        return (<button {...p} ref={(root)=>{this.root = root;}} className={`xz-btn xz-btn-size-${Theme.getConfig('size',this.props)} xz-btn-type-${Theme.getConfig('btn-type',this.props)}`}>
+        return (<button {...p} ref={(root)=>{this.root = root;}} className={className.join(' ')}>
                 {this.props.children}
             </button>)
     }
