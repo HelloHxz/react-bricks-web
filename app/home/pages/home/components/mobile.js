93e39cb318cac4603cecb2066b1deaf94b480995
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2018-07-29 08:40:47 +0800

    -

--- app/home/pages/home/components/mobile.js
+++ app/home/pages/home/components/mobile.js
@@ -1,20 +1,24 @@
 import {React,PageView,Image,Tabs,Input,Form,Select,RouteView,Row,Modal,Button,VBox,HBox,XZ} from "react-bricks-web"
 import './mobile.less';
-import BkImage from '../../../imgs/mobile-bk.jpg';
 
 class HomeScreen extends React.PureComponent {
+  renderWTFPL(){
+    const spans = [];
+    for(let i=0;i<1000;i+=1){
+      spans.push(<span key={i}>WTFPL</span>);
+    }
+    return <div className='wtfpl'>{spans}</div>;
+  }
 
   render() {
     return (
         <React.Fragment>
           <div className='mobile-top'>
-            <Image style={{ width:'100%' ,height:'100%',zIndex:1 }} src={BkImage} resizeMode="contain" />
-            <div className='mobile-top-wrapper'>
-                <p className='mobile-top-title'>React Bricks Mobile</p>
-                <p className='mobile-top-desc'>专注移动端组件封装,用于构建专业的手机端移动应用,
-                一套代码构建IOS.Android.HTML5应用,
-                是我们多年企业开发经验以及集各大家之所长之所得,让开发者更好的开发体验或从其中得到一些启示</p>
-            </div>
+              {this.renderWTFPL()}
+              <p className='mobile-top-title'>React Bricks Mobile</p>
+              <p className='mobile-top-desc'>专注移动端组件封装,用于构建专业的手机端移动应用,
+              一套代码构建IOS.Android.HTML5应用,
+              是我们多年企业开发经验以及集各大家之所长之所得,让开发者更好的开发体验或从其中得到一些启示</p>
           </div>
         </React.Fragment>
     );
--- app/home/pages/home/components/web.js
+++ app/home/pages/home/components/web.js
@@ -1,18 +1,21 @@
 import {React,PageView,Image,Tabs,Input,Form,Select,RouteView,Row,Modal,Button,VBox,HBox,XZ} from "react-bricks-web"
 import './web.less';
-import BkImage from '../../../imgs/web-bk.jpg';
 
 class HomeScreen extends React.PureComponent {
-
+    renderWTFPL(){
+        const spans = [];
+        for(let i=0;i<1000;i+=1){
+            spans.push(<span key={i}>WTFPL</span>);
+        }
+        return <div className='wtfpl'>{spans}</div>;
+    }
   render() {
     return (
         <React.Fragment>
           <div className='web-top'>
-            <Image style={{ width:'100%' ,height:'100%',zIndex:1 }} src={BkImage} resizeMode="contain" />
-            <div className='web-top-wrapper'>
-                <p className='web-top-title'>React Bricks Web</p>
-                <p className='web-top-desc'>专注PC Web 端组件封装,用于构建专业的企业中后台管理应用,是我们多年企业开发经验以及集各大家之所长之所得,让开发者更好的开发体验或从其中得到一些启示</p>
-            </div>
+            <p className='web-top-title'>React Bricks Web</p>
+            <p className='web-top-desc'>专注PC Web 端组件封装,用于构建专业的企业中后台管理应用,是我们多年企业开发经验以及集各大家之所长之所得,让开发者更好的开发体验或从其中得到一些启示</p>
+            {this.renderWTFPL()}
           </div>
         </React.Fragment>
     );
